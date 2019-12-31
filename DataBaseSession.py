a16a898e615815dd9b1c847c1da0fceace46cd96
royjan <roy@jan.co.il>
2019-12-31 08:23:14 +0200

    deleted script from file (CreateTable)

--- DataBaseSession.py
+++ DataBaseSession.py
@@ -10,37 +10,3 @@ class DataBaseSession:
                            connect_args=dict(use_unicode=True), pool_size=5)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-
-"""
-CREATE STATEMENT:
-
-CREATE DATABASE `jsp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-CREATE TABLE `car_mapping` (
-  `num_vehicle` text,
-  `vin` char(255) NOT NULL,
-  PRIMARY KEY (`vin`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `part_mapping` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `part_name` varchar(256) NOT NULL,
-  `car_name` varchar(256) DEFAULT NULL,
-  `part_number` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `part_name` (`part_name`),
-  CONSTRAINT `part_mapping_ibfk_1` FOREIGN KEY (`part_name`) REFERENCES `parts` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `parts` (
-  `name` varchar(255) NOT NULL,
-  `cat` text,
-  `section` text,
-  `line` text,
-  `original_part_name` text,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-"""
